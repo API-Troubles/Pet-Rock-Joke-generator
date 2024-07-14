0c8710398adc3bc3655861b4800c6b75059bab13
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-14 16:27:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+//Get jokes from a json file, thanks ChatGPT for the jokes!
+const jokesJson = httpGet("/jokes.json");
+const obj = JSON.parse(jokesJson);
+
+//Get joke
+var joke = obj[Math.floor(Math.random()*obj.length)]["joke"];
