2e3f09a43be064125173ae066160d76dda90ea12
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-14 17:39:01 +0000

    BUGFIX: Changed joke to be added on DOMContentLoad

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function httpGet(theUrl)
 //Get jokes from a json file, thanks ChatGPT for the jokes!
 const joke_json = httpGet("/jokes.json")
 const obj = JSON.parse(joke_json);
+var joke = obj[Math.floor(Math.random() * obj.length)]["joke"];
 
-//Get jokes lol
-var joke = obj[Math.floor(Math.random()*obj.length)]["joke"];
-document.getElementById("jokeDialogueBox").innerHTML = joke;
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("jokeDialogueBox").innerHTML = joke;
+});
